be3c3c170cd1f727de65b77bd53d7cee574d93f5
rafaelswr <rafaels.borges91@gmail.com>
2024-07-15 16:02:32 +0100

    initial

---
+++ src/main/java/com/rafaelswr/MasteringSpring1/MasteringSpring1Application.java
@@ -0,0 +1,13 @@
+package com.rafaelswr.MasteringSpring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteringSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteringSpring1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelswr/MasteringSpring1/MasteringSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelswr.MasteringSpring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasteringSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
