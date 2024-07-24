3313e7b78a0d41405d85775e1417c613ab477c3b
rafaelswr <rafaels.borges91@gmail.com>
2024-07-24 18:13:01 +0100

    Json Property annotation by object key
    map object's key with jsonProperty in atributes models

--- src/main/java/com/rafaelswr/ExampleRest/Model/Order.java
+++ src/main/java/com/rafaelswr/ExampleRest/Model/Order.java
@@ -1,5 +1,6 @@
 package com.rafaelswr.ExampleRest.Model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,13 +9,26 @@ import lombok.NoArgsConstructor;
 @Data
 public class Order {
 
+    @JsonProperty("c-name")
     private String customerName;
+
+    @JsonProperty("p-name")
     private String productName;
+
     private int quantity;
 
     public Order(String customerName, String productName, int quantity) {
         this.customerName = customerName;
         this.productName = productName;
         this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "customerName='" + customerName + '\'' +
+                ", productName='" + productName + '\'' +
+                ", quantity=" + quantity +
+                '}';
     }
 }
